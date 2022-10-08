40ef972f632ed9b8c2864d1d2e67b45e04e9ae5e
Hamza Ramzanali <hamzaramzanali@Hamzas-MacBook-Air.local>
2022-10-07 23:36:37 -0700

    added routes, models, and seeds

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,6 +9,33 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    product_price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+
+    }, 
+    product_stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Category',
+        key: 'id',
+      },
+    }
     // define columns
   },
   {
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,22 +6,22 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: ''
+  foreignKey: 'product_id'
 })
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: ''
+  foreignKey: 'category_id'
 })
 // Products belongToMany Tags (through ProductTag)
 
 Product.belongsToMany(Tag, {
-  foreignKey:''
+  through: ProductTag
 })
 // Tags belongToMany Products (through ProductTag)
 
 Tag.belongsToMany(Product, {
-  foreignKey: ''
+  through: ProductTag
 })
 
 module.exports = {

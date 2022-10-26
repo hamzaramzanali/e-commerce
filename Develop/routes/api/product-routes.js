08dbd2f737479e582f108784bae044ab9383655c
Hamza Ramzanali <hamzaramzanali@Hamzas-MacBook-Air.local>
2022-10-25 19:28:19 -0500

    fixed all routes and models to allow posting and deleting and updating

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -44,7 +44,7 @@ router.post('/', (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      if (req.body.tagIds) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
@@ -105,8 +105,21 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deleteProduct = await Product.destroy({
+      where: {
+        id: req.params.id
+    },
+  });
+    if (!deleteProduct) {
+      res.status(404).json({ message: "No products found with that ID"})
+    }
+    res.status(200).json(deleteProduct)
+  }catch(err){
+  res.status(500).json(err)
+  }
 });
 
 module.exports = router;
